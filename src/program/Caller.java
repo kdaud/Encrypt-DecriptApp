b93abc0b641595c7b790e3af30689303fc5687f9
kdaud <dkakumirizii@gmail.com>
2020-10-23 23:04:05 +0300

    coding

--- src/program/Caller.java
+++ src/program/Caller.java
@@ -7,6 +7,7 @@ public class Caller {
 	public static void main(String[] args) {
 		Networking obj = new Networking("Daud", "Kakumirizi");
 		System.out.println(obj.name + " " + obj.lname);
+		System.out.println("Hello Programming");
 		obj.multiply(3, 6, 6);
 	}
 	
