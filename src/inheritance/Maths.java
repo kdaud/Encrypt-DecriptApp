dbfe1ac9acd43ec7b1ba60b81faf34d7ddc158c7
kdaud <dkakumirizii@gmail.com>
2020-10-23 23:18:31 +0300

    myCodes

---
+++ src/inheritance/Maths.java
@@ -0,0 +1,10 @@
+package inheritance;
+
+
+public class Maths {
+	
+	public String biodata(String name, String cuntry) {
+		return name+"  "+"comes from " +cuntry;
+	}
+	
+}
---
+++ src/inheritance/Printer.java
@@ -0,0 +1,12 @@
+package inheritance;
+
+
+public class Printer {
+	
+	public static void main(String[] args) {
+		Maths objMaths = new Maths();
+		String tf = objMaths.biodata("Daud", "Uganda");
+		System.out.println(tf);
+	}
+	
+}
