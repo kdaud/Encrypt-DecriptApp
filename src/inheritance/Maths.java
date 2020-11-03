5894148095c157f32d725685441a535598859a2c
kdaud <dkakumirizii@gmail.com>
2020-11-03 21:19:55 +0300

    Updated hard coded stuffs

--- src/inheritance/Maths.java
+++ src/inheritance/Maths.java
@@ -5,6 +5,11 @@ public class Maths {
 	
 	public String biodata(String name, String cuntry) {
 		return name+"  "+"comes from " +cuntry;
+		
+	}
+	
+	static {
+		System.out.println("My Google Journey!");
 	}
 	
 }
